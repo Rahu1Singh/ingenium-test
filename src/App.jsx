eade1723e0067c45a495030738b7d94655f9a7c0
Rahu1Singh <srahul1357@gmail.com>
2024-07-02 10:33:37 +0530

    using navlink in hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { HashRouter as Router , Routes , Route} from 'react-router-dom'
+import { BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 
 import Home from './pages/Home'
 import About from './pages/About'
