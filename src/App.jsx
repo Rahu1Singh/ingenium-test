ba88cb95cb1a5ea2f0d4129a9c1aeb7e9f6c570c
Rahu1Singh <91598280+Rahu1Singh@users.noreply.github.com>
2024-07-02 11:12:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -24,6 +25,7 @@ import FieldDevices from './pages/FieldDevices';
 export default function App() {
   return (
     <Router>
+      <ScrolltoTop />
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
