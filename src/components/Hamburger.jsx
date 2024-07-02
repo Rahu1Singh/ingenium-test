22dd9e3b55f158712a65b61a7cb020cb81547f62
Rahu1Singh <srahul1357@gmail.com>
2024-07-02 10:43:28 +0530

    making hamburger work

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { navbaritems } from "../constants";
 
 const Hamburger = () => {
-    return (
-        <div className="absolute p-5 z-50 flex flex-col items-center justify-center h-screen w-full space-y-10 mb-10">
-            {navbaritems.map((item) => (
-                <div key={item.id} className="w-full flex flex-col items-center">
-                    <Link href={item.url} className="block uppercase font-medium text-xl w-full text-center hover:font-bold hover:text-b-800 transition-colors">
-                        {item.title}
-                    </Link>
-                    {item.id !== "4" && <div className="w-4/5 border-b-2 border-b-700 py-5" />}
-                </div>
-            ))}
+  return (
+    <div className="absolute p-5 z-50 flex flex-col items-center justify-center h-screen w-full space-y-10 mb-10">
+      {navbaritems.map((item) => (
+        <div key={item.id} className="w-full flex flex-col items-center">
+          <NavLink 
+            to={item.url} 
+            className="block uppercase font-medium text-xl w-full text-center hover:font-bold hover:text-b-800 transition-colors"
+          >
+            {item.title}
+          </NavLink>
+          {item.id !== "4" && <div className="w-4/5 border-b-2 border-b-700 py-5" />}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Hamburger;
