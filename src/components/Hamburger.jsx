c45001c6460112523144887bc4ee4a386a2fa7ae
Rahu1Singh <91598280+Rahu1Singh@users.noreply.github.com>
2024-07-02 10:38:03 +0530

    changed navlink to link

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { navbaritems } from "../constants";
 
 const Hamburger = () => {
@@ -6,9 +6,9 @@ const Hamburger = () => {
         <div className="absolute p-5 z-50 flex flex-col items-center justify-center h-screen w-full space-y-10 mb-10">
             {navbaritems.map((item) => (
                 <div key={item.id} className="w-full flex flex-col items-center">
-                    <NavLink href={item.url} className="block uppercase font-medium text-xl w-full text-center hover:font-bold hover:text-b-800 transition-colors">
+                    <Link href={item.url} className="block uppercase font-medium text-xl w-full text-center hover:font-bold hover:text-b-800 transition-colors">
                         {item.title}
-                    </NavLink>
+                    </Link>
                     {item.id !== "4" && <div className="w-4/5 border-b-2 border-b-700 py-5" />}
                 </div>
             ))}
